55c7f38b3fed1138d945a0f703d6bb6623ebc1dd
Rajdeep Panwar <rajdeepsinghpanwar972@gmail.com>
2025-01-02 18:40:00 +0530

    pass expected result for 3

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
-  def add(s)
-    s.split(',').map(&:to_i).sum
+  def add(str)
+  	str = str.gsub(/\n/, ',') # gsub replace \n with , and below code will remain same
+    str.split(',').map(&:to_i).sum
   end
 end
