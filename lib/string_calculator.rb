26deb8b02e7e2bfa75df276c6d409ad7dd4c269e
Rajdeep Panwar <rajdeepsinghpanwar972@gmail.com>
2025-01-02 19:09:36 +0530

    pass rspc for test 5

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,9 @@
 class StringCalculator
   def add(str)
-  	str = str.scan(/\d+/) # let's use scan for fetching numbers from string
+    str = str.scan(/-?\d+/) # let's use scan for fetching numbers from string
+    str.map do |num|
+      raise "negatives not allowed: #{num}" if num.to_i < 0
+    end
     str.map(&:to_i).sum # no need split scan default retun array
   end
 end
