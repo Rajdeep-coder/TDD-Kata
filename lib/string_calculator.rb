62eacc6b2106c59cb4de273c5229c788df4f1825
Rajdeep Panwar <rajdeepsinghpanwar972@gmail.com>
2025-01-02 18:15:47 +0530

    Pass the rspecs for 2

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,10 +1,5 @@
 class StringCalculator
-	def add(s)
-		a = s.split(',')
-		a1 = a[0].to_i
-		a2 = a[1].to_i
-		a3 = a[2].to_i
-
-		a1 + a2 + a3
-	end
+  def add(s)
+    s.split(',').map(&:to_i).sum
+  end
 end
